2faec39503acdbfe5f6ba11a2ac33782c66f7376
grosoon <greyson.bourgeois@gmail.com>
2015-11-09 20:05:10 -0600

    Pressure added
    pressure code added

--- comp/src/sensors.h
+++ comp/src/sensors.h
@@ -6,9 +6,14 @@
  *
  * (c) 2015.  All Rights Reserved.
  */
+#include <SFE_BMP180.h>
+#include <Wire.h>
+
 
 double getLatitude();
 double getLongitude();
 double getAltitude();
 double getTemperature();
 double getPressure();
+
+void sensorSetup();
